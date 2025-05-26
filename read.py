5ac96be6c82b2c5ad8af443c7e33c49080487555
Sana-Harshitha <sanaharshitha2@gmail.com>
2025-05-26 13:01:49 +0530

    used rescaling function while showing the image

--- read.py
+++ read.py
@@ -2,6 +2,13 @@ import cv2 as cv
 
 profile=cv.imread('images/profile.jpg')
 
-cv.imshow("User", profile)
 
+def rescaleFrame(frame,scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions=(width,height)
+
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
+cv.imshow("User", rescaleFrame(profile,0.5))
 cv.waitKey(0)
